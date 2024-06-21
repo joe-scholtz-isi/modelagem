6ac6f389dc3fde4a01f7954bf7c7680af866017e
Joe Ferreira Scholtz <joe.scholtz@senairs.org.br>
2024-06-21 16:29:16 -0300

    add analise_modelos

--- cinematica_direta.m
+++ cinematica_direta.m
@@ -1,11 +1,9 @@
 function [x2p, y2p, wp] = dinamica_direta(Vx, Vy, w, F1, F2, F3, F4, s1, s2, s3, s4)
-  vxy = [Vx1; Vy1; Vx2; Vy2; Vx3; Vy3; Vx4; Vy4];    
+  vxy = [Vx1; Vy1; Vx2; Vy2; Vx3; Vy3; Vx4; Vy4];
 
-  sigma = [1,   0,   1,    0,   1,   0,    1,    0;
-
-           0,   1,   0,    1,   0,   1,    0,    1;
-
-           l/2, c/2, -l/2, c/2, l/2, -c/2, -l/2, -c/2];
+  sigma = [1, 0, 1, 0, 1, 0, 1, 0;
+           0, 1, 0, 1, 0, 1, 0, 1;
+           l/2, c/2, -l/2, c/2, l/2, -c/2, -l/2, -c/2];
 
   x=sigma*vxy;
 
